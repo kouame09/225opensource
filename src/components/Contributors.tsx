c34acfa43070e7394355d102ba6c52d28e44c880
kouame09 <princekouame7@gmail.com>
2025-10-03 14:04:21 +0000

    Add

--- src/components/Contributors.tsx
+++ src/components/Contributors.tsx
@@ -65,8 +65,20 @@ function Contributors() {
               <img
                 src={contributor.avatar}
                 alt={contributor.name}
-                className="relative w-12 h-12 rounded-full object-cover ring-2 ring-white shadow-lg hover:scale-110 transition-transform duration-300"
+                className="relative w-12 h-12 rounded-full object-cover ring-2 ring-white shadow-lg hover:scale-110 transition-transform duration-300 cursor-pointer"
               />
+
+              {/* Popup tooltip */}
+              <div className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 opacity-0 group-hover:opacity-100 transition-all duration-300 pointer-events-none z-10">
+                {/* Flèche du tooltip */}
+                <div className="absolute top-full left-1/2 transform -translate-x-1/2 w-0 h-0 border-l-4 border-r-4 border-t-4 border-transparent border-t-green-600 dark:border-t-green-500"></div>
+
+                {/* Contenu du tooltip */}
+                <div className="bg-green-600 dark:bg-green-500 text-white dark:text-white text-xs px-3 py-2 rounded-lg shadow-lg whitespace-nowrap min-w-max">
+                  <div className="font-semibold">{contributor.name}</div>
+                  <div className="text-green-200 dark:text-green-100 text-xs">{contributor.role}</div>
+                </div>
+              </div>
             </div>
           ))}
         </div>
